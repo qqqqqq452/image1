67a521bbb44d1f952fb9e2d7c8514d635792fa03
Vorozhtsov_Kirill <kvorozhtsov@mail.ru>
2022-05-02 10:19:51 +0500

    7

--- index.py
+++
@@ -1,7 +0,0 @@
-def handler(event, context):
-    print("\U0001F4C2 Starting function after trigger")
-    print(event)     
-    return {
-        'statusCode': 200,
-        'body': 'File is uploaded',
-    }
